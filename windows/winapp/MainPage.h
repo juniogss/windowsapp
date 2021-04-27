185f0eb1dd23fab15cb6aefa4cf94f1a50ca470b
Junio Gabriel <junio.gss@hotmail.com>
2021-04-27 00:06:33 -0300

    first commit

---
+++ windows/winapp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::winapp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::winapp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
